f0c0dd8bce9bf6d9e5be042e3a1e7f139fae251a
Vlad-coder-678 <vova.mezencev@rambler.ru>
2021-07-09 17:26:07 +0300

    add section 'Services'

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import React from "react";
 
 import GlobalStyle from "./GlobalStyle";
 import Header from "./components/Header";
+import Services from "./components/Services";
+
+import { servicesData } from "./data/servicesData";
 
 function App() {
   return (
     <>
       <GlobalStyle />
       <Header />
+      <Services servicesData={servicesData} />
     </>
   );
 }
---
+++ src/data/servicesData.js
@@ -0,0 +1,90 @@
+import Image1 from "../images/services_10.jpg";
+import Image2 from "../images/services_2.jpg";
+import Image3 from "../images/services_3.jpg";
+import Image4 from "../images/services_4.jpg";
+
+export const servicesData = [
+  {
+    title: "Аудит и анализ бизнеса",
+    price: "50,000 руб",
+    path: "/homes",
+    label: "Подробнее",
+    image: Image1,
+    alt: "Аудит и анализ бизнеса",
+    sayThanks: `Photo by <a href="https://unsplash.com/@kmuza?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Carlos Muza</a> on <a href="https://unsplash.com/s/photos/business?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    `,
+  },
+  {
+    title: "Консалт",
+    price: "50,000 руб",
+    path: "/homes",
+    label: "Подробнее",
+    image: Image2,
+    alt: "Консалт",
+    sayThanks: `Photo by <a href="https://unsplash.com/@naomi365photography?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">vu anh</a> on <a href="https://unsplash.com/s/photos/house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    `,
+  },
+  {
+    title: "Окей анализ",
+    price: "50,000 руб",
+    path: "/homes",
+    label: "Подробнее",
+    image: Image3,
+    alt: "Окей анализ",
+    sayThanks: `Photo by <a href="https://unsplash.com/@heybeautimagazine?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Nicola Styles</a> on <a href="https://unsplash.com/s/photos/okay?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    `,
+  },
+  {
+    title: "Финансовый рост",
+    price: "50,000 руб",
+    path: "/homes",
+    label: "Подробнее",
+    image: Image4,
+    alt: "Финансовый рост",
+    sayThanks: `Photo by <a href="https://unsplash.com/@micheile?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Micheile Henderson</a> on <a href="https://unsplash.com/s/photos/finance?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    `,
+  },
+];
+
+// export const SliderDataTwo = [
+//   {
+//     title: "House in Las Vegas, Nevada",
+//     price: "$8,980,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: Image5,
+//     alt: "House",
+//     sayThanks: `Photo by <a href="https://unsplash.com/@sidekix?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Sidekix Media</a> on <a href="https://unsplash.com/s/photos/house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//     `,
+//   },
+//   {
+//     title: "House in Houston, Texas",
+//     price: "$3,460,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: Image6,
+//     alt: "House",
+//     sayThanks: `Photo by <a href="https://unsplash.com/@lycs?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">LYCS Architecture</a> on <a href="https://unsplash.com/s/photos/house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//     `,
+//   },
+//   {
+//     title: "House in Alajuela, Costa Rica",
+//     price: "$5,420,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: Image7,
+//     alt: "House",
+//     sayThanks: `Photo by <a href="https://unsplash.com/@johnfo?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">John Fornander</a> on <a href="https://unsplash.com/s/photos/house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//     `,
+//   },
+//   {
+//     title: "Luxury Villa in Balli. Indonesia",
+//     price: "$4,280,000",
+//     path: "/homes",
+//     label: "View Home",
+//     image: Image8,
+//     alt: "House",
+//     sayThanks: `Photo by <a href="https://unsplash.com/@ciudadmaderas?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ciudad Maderas</a> on <a href="https://unsplash.com/s/photos/house?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+//     `,
+//   },
+// ];
